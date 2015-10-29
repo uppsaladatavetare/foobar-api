c5102b608e95607330a4f37c3d123c5261e51225
Krzysztof Jagiello <me@kjagiello.com>
2015-10-29 15:42:06 +0100

    Include the wallet balance in the account details

--- src/foobar/rest/serializers/account.py
+++ src/foobar/rest/serializers/account.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
+from foobar.wallet import api as wallet_api
+from ..fields import MoneyField
 
 
 class AccountSerializer(serializers.Serializer):
+    def get_balance(self, instance):
+        _, balance = wallet_api.get_balance(instance.id)
+        return MoneyField().to_representation(balance)
+
     id = serializers.UUIDField(read_only=True)
     user_id = serializers.UUIDField(read_only=True, source='user.id')
+    balance = serializers.SerializerMethodField()
