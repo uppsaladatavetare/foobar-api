134adcc2b4e99da5586becde3bc8f97ed4bf9168
Petter Friberg <petter_friberg@hotmail.com>
2017-02-08 14:17:30 +0100

    Updated settings and urls to work with debug toolbar locally

--- src/foobar/settings/local.py
+++ src/foobar/settings/local.py
@@ -1,1 +1,6 @@
-from .base import *  # noqa
+from .base import *
+from bananas.url import database_conf_from_url
+
+INTERNAL_IPS = ['127.0.0.1']
+INSTALLED_APPS += ('debug_toolbar',)
+MIDDLEWARE_CLASSES += ('debug_toolbar.middleware.DebugToolbarMiddleware',)
