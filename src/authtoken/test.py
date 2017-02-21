65d5039aa2363387158b8c3f5c770485e52233ee
Petter Friberg <petter_friberg@hotmail.com>
2017-02-21 20:01:33 +0100

    Updated tests to utilize mock data in library responses. Also removed tests actually testing those libraries

--- src/authtoken/test.py
+++ src/authtoken/test.py
@@ -1,5 +1,6 @@
 import datetime
 import jwt
+from unittest.mock import patch
 
 from django.conf.urls import url
 from django.http import HttpResponse
@@ -12,7 +13,6 @@ from rest_framework.views import APIView
 from .authentication import FooCardAuthentication
 from .models import Token
 from .views import obtain_foocard_token
-from foobar.models import Account
 from foobar.tests.factories import AccountFactory, CardFactory
 
 factory = APIRequestFactory()
@@ -44,14 +44,19 @@ class AuthenticateFooCardTokenTests(TestCase):
     def setUp(self):
         self.csrf_client = APIClient(enforce_csrf_checks=True)
 
-    def test_correct_auth_of_token(self):
-        account = AccountFactory()
+    @patch('authtoken.authentication.jwt.decode')
+    def test_correct_auth_of_token(self, mock_jwt):
+        card = CardFactory()
         token = jwt.encode(
-            {'account_id': str(account.pk)},
+            {'card_id': card.number},
             'secret',
             algorithm=self.enc
         )
-        auth = 'CardToken {0}'.format(token.decode())
+        token = token.decode()
+        stamp = timezone.now() + timezone.timedelta(minutes=5)
+        mock_jwt.return_value = {'exp': stamp, 'card_id': card.number}
+
+        auth = 'CardToken {0}'.format(token)
         response = self.csrf_client.post(
             self.path,
             {'example': 'example'},
@@ -60,87 +65,46 @@ class AuthenticateFooCardTokenTests(TestCase):
         )
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        mock_jwt.assert_called_once_with(token, 'secret', algorithm=self.enc)
 
-    def test_auth_with_invalid_token(self):
-        token = jwt.encode(
-            {'some': 'payload'},
-            'invalidsecret',
-            algorithm=self.enc
-        )
-        auth = 'CardToken {0}'.format(token.decode())
-        response = self.csrf_client.post(
-            self.path,
-            {'example': 'example'},
-            format='json',
-            HTTP_AUTHORIZATION=auth
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_auth_with_expired_token(self):
-        stamp = timezone.now() - datetime.timedelta(minutes=10)
-        token = jwt.encode({'exp': stamp}, 'secret', algorithm=self.enc)
-        auth = 'CardToken {0}'.format(token.decode())
-        response = self.csrf_client.get(
-            self.path,
-            HTTP_AUTHORIZATION=auth
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
-
-    def test_auth_with_bad_account(self):
+    @patch('authtoken.authentication.jwt.decode')
+    def test_auth_with_bad_account(self, mock_jwt):
         AccountFactory()  # Keep this here so we just don't have an empty QS
         stamp = timezone.now() + datetime.timedelta(minutes=10)
         token = jwt.encode(
-            {'exp': stamp, 'account_id': None},
+            {'exp': stamp, 'card_id': None},
             'secret',
             algorithm=self.enc
         )
-        auth = 'CardToken {0}'.format(token.decode())
-        response = self.csrf_client.get(
-            self.path,
-            {'example': 'example'},
-            format='json',
-            HTTP_AUTHORIZATION=auth
-        )
+        token = token.decode()
+        mock_jwt.return_value = {'exp': stamp, 'card_id': None}
+
+        auth = 'CardToken {0}'.format(token)
+        response = self.csrf_client.get(self.path, HTTP_AUTHORIZATION=auth)
 
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        mock_jwt.assert_called_once_with(token, 'secret', algorithm=self.enc)
 
     def test_auth_with_bad_header(self):
         account = AccountFactory()
         stamp = timezone.now() + datetime.timedelta(minutes=10)
         token = jwt.encode(
-            {'exp': stamp, 'account_id': str(account.pk)},
+            {'exp': stamp, 'card_id': str(account.pk)},
             'secret',
             algorithm=self.enc
         )
         auth = 'ThisIsIncorrect {0}'.format(token.decode())
-        response = self.csrf_client.get(
-            self.path,
-            {'exp': stamp, 'account_id': str(account.pk)},
-            format='json',
-            HTTP_AUTHORIZATION=auth
-        )
+        response = self.csrf_client.get(self.path, HTTP_AUTHORIZATION=auth)
         # Returns 200_OK as it is not allowed to even try to authenticate
         # And there is no other authentication that is tested
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         auth = 'CardToken'
-        response = self.csrf_client.get(
-            self.path,
-            {'exp': stamp, 'account_id': str(account.pk)},
-            format='json',
-            HTTP_AUTHORIZATION=auth
-        )
+        response = self.csrf_client.get(self.path, HTTP_AUTHORIZATION=auth)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
         auth = 'CardToken {0} {1}'.format(token.decode()[:10], token.decode())
-        response = self.csrf_client.get(
-            self.path,
-            {'exp': stamp, 'account_id': str(account.pk)},
-            format='json',
-            HTTP_AUTHORIZATION=auth
-        )
+        response = self.csrf_client.get(self.path, HTTP_AUTHORIZATION=auth)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -155,6 +119,7 @@ class CreateFooCardTokenTests(TestCase):
     def test_create_new_account_token(self):
         card = CardFactory()
         api_token = Token.objects.create(key='abc123')
+
         auth = 'Token {0}'.format(api_token)
         response = self.csrf_client.post(
             self.path,
@@ -170,14 +135,9 @@ class CreateFooCardTokenTests(TestCase):
         self.assertIsNotNone(token)
         self.assertTrue(len(token) > 0)
 
-        try:
-            decoded = jwt.decode(token, 'secret', algorithm=self.enc)
-        except jwt.exceptions.InvalidTokenError as e:
-            self.fail(e)
-
-        account = Account.objects.all().first()
-        self.assertEqual(decoded.get('account_id'), str(account.pk))
-        self.assertIn('exp', decoded.keys())
+        auth = 'CardToken {0}'.format(token)
+        response = self.csrf_client.get('/cardtoken/', HTTP_AUTHORIZATION=auth)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_create_token_for_invalid_account(self):
         api_token = Token.objects.create(key='abc123')
@@ -185,39 +145,6 @@ class CreateFooCardTokenTests(TestCase):
         response = self.csrf_client.post(
             self.path,
             {'number': '123456'},
-            format='json',
-            HTTP_AUTHORIZATION=auth
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn('number', response.data.keys())
-
-    def test_create_token_with_incorrect_data(self):
-        api_token = Token.objects.create(key='abc123')
-        auth = 'Token {0}'.format(api_token)
-        response = self.csrf_client.post(
-            self.path,
-            {'sumtinwong': '123456'},
-            format='json',
-            HTTP_AUTHORIZATION=auth
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn('number', response.data.keys())
-
-        response = self.csrf_client.post(
-            self.path,
-            {'number': ''},
-            format='json',
-            HTTP_AUTHORIZATION=auth
-        )
-
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertIn('number', response.data.keys())
-
-        response = self.csrf_client.post(
-            self.path,
-            {'number': None},
             format='json',
             HTTP_AUTHORIZATION=auth
         )
