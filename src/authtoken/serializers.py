b20a85f05703a7cfc9f250347dc07dc5f1456d45
Petter Friberg <petter_friberg@hotmail.com>
2017-02-21 19:00:37 +0100

    Validating card number on field level, with help from the internal API

--- src/authtoken/serializers.py
+++ src/authtoken/serializers.py
@@ -7,11 +7,10 @@ from foobar.api import get_account
 class CardTokenSerializer(serializers.Serializer):
     number = serializers.CharField(required=True, allow_blank=False)
 
-    def validate(self, attrs):
-        account = get_account(attrs.get('number'))
+    def validate_number(self, value):
+        account = get_account(value)
         if account is None:
             msg = {'number': _('Card is not registered')}
             raise serializers.ValidationError(msg)
 
-        attrs['account'] = account
-        return attrs
+        return value
