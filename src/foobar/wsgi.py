7f20f80f2a3f90ed222702f9486dc3d76e76251b
Krzysztof Jagiello <me@kjagiello.com>
2017-03-08 12:05:59 +0100

    Catch the exceptions at wsgi level

--- src/foobar/wsgi.py
+++ src/foobar/wsgi.py
@@ -10,5 +10,6 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foobar.settings.local")
 
+from raven.contrib.django.raven_compat.middleware.wsgi import Sentry  # NOQA
 from django.core.wsgi import get_wsgi_application  # NOQA
-application = get_wsgi_application()
+application = Sentry(get_wsgi_application())
