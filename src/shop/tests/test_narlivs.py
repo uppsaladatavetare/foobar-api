f55721a79fed0be29c5d86c2a9e87727bad6f6ea
Krzysztof Jagiello <me@kjagiello.com>
2017-03-08 11:08:37 +0100

    Correcting the computation of the net price based on pant

--- src/shop/tests/test_narlivs.py
+++ src/shop/tests/test_narlivs.py
@@ -8,14 +8,26 @@ TESTDATA_DIR = join(dirname(abspath(__file__)), 'data')
 class NarlivsTest(TestCase):
     def setUp(self):
         self.api = get_supplier_api('narlivs')
-        self.report_path = join(TESTDATA_DIR, 'delivery_report.pdf')
+
+    def report_path(self, name):
+        return join(TESTDATA_DIR, name)
 
     def test_pdf_to_text(self):
-        text = narlivs.pdf_to_text(self.report_path)
+        text = narlivs.pdf_to_text(self.report_path('delivery_report.pdf'))
         self.assertTrue('001337' in text)
 
     def test_receive_delivery(self):
-        items = self.api.parse_delivery_report(self.report_path)
+        api = get_supplier_api('narlivs')
+        path = self.report_path('delivery_report.pdf')
+        items = api.parse_delivery_report(path)
         self.assertEqual(len(items), 32)
+        for item in items:
+            self.assertEqual(len(item.sku), 9)
+
+    def test_receive_delivery2(self):
+        api = get_supplier_api('narlivs')
+        path = self.report_path('delivery_report2.pdf')
+        items = api.parse_delivery_report(path)
+        self.assertEqual(len(items), 41)
         for item in items:
             self.assertEqual(len(item.sku), 9)
